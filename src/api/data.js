b0646ceb8124b76bc72cad6184fd834b93b535ce
sg1345 <dimitkostz@gmail.com>
2026-06-11 07:03:42 +0200

    Update API host configuration to use window.API_URL

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
-const host = 'http://localhost:3030'
-api.settings.host = 'http://localhost:3030';
+const host = window.API_URL || 'http://localhost:3030';
+api.settings.host = window.API_URL || 'http://localhost:3030';
 
 export const login = api.login;
 export const register = api.register;
